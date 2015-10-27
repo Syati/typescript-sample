0e48b41022620c11356092b1d34c12b49a2fee4b
mizuki-y <mizuki-y@syati.info>
2015-10-28 08:32:03 +0900

    Add 05 functions

--- 05_react_redux_counter/app/src/components/Counter.tsx
+++ 05_react_redux_counter/app/src/components/Counter.tsx
@@ -2,22 +2,18 @@ import * as React from 'react';
 
 class Counter extends React.Component<any, any> {
     render() {
-        const { increment, decrement, counter} = this.props;
+        const { increment, incrementIfOdd, incrementAsync, decrement, counter} = this.props;
 
         return (
             <p>
                 Clicked: {counter}
                 <button onClick={increment}>+</button>
                 <button onClick={decrement}>-</button>
+                <button onClick={incrementIfOdd}>Increment if odd</button>
+                <button onClick={() => incrementAsync()}>Increment async</button>
             </p>
         );
     }
-}
-
-namespace Counter {
-    export interface Props {}
-
-    export interface State {}
 }
 
 export default Counter;
