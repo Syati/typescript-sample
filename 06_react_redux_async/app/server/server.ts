9241b2a0be54e1c5e59c24cbd5a3b133a87d251d
mizuki-y <mizuki-y@syati.info>
2015-11-02 19:26:54 +0900

    Remove debug lib

--- 06_react_redux_async/app/server/server.ts
+++ 06_react_redux_async/app/server/server.ts
@@ -1,11 +1,9 @@
 
 import * as express from 'express';
-import * as debug from 'debug';
 import config from './config';
 import settings from './settings';
 
 let app = express();
-let logger: debug.Debugger = debug('server');
 
 config(app);
 
